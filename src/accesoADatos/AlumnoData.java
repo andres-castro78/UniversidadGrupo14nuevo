b2006e7c507518dec97b3e319b03174fbbbb9465
andres-castro78 <castroandres191078@gmail.com>
2023-09-11 15:38:14 -0300

    Cambiado

--- src/accesoADatos/AlumnoData.java
+++ src/accesoADatos/AlumnoData.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package accesoADatos;
 
-/**
- *
- * @author recre
- */
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class AlumnoData {
     
 }
