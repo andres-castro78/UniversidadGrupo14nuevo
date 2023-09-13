14ab19a79ec22be7712cd7d6da0fb661d38c6dbe
L3tt1 <barboza.leticia@gmail.com>
2023-09-13 13:51:40 -0300

    Clase MateriaData
    método agregarMateria()

--- src/universidadgrupo14nuevo/UniversidadGrupo14nuevo.java
+++ src/universidadgrupo14nuevo/UniversidadGrupo14nuevo.java
@@ -9,18 +9,18 @@ public class UniversidadGrupo14nuevo {
 
     public static void main(String[] args) {
         
-       Alumno juan = new Alumno (1, 12345678, "Perez", "Rodrigo",LocalDate.of(1970, 5 , 17),true);
+       Alumno juan = new Alumno (1, 111111111, "Luna", "Juan Pedro",LocalDate.of(1970, 5 , 17),false);
        AlumnoData alu = new AlumnoData();
-       //alu.guardarAlumno(juan); 
+       alu.guardarAlumno(juan); 
       //alu.modificarAlumno(juan);
       //alu.eliminarAlumno(1);
-       Alumno alumnoEncontrado = alu.buscarAlumno(5);
-        if(alumnoEncontrado!=null){
-        System.out.println("Apellido: " + alumnoEncontrado.getApellido());
-        System.out.println("Nombre: " + alumnoEncontrado.getNombre());
-        System.out.println("DNI: " + alumnoEncontrado.getDni());
+//       Alumno alumnoEncontrado = alu.buscarAlumno(5);
+//        if(alumnoEncontrado!=null){
+//        System.out.println("Apellido: " + alumnoEncontrado.getApellido());
+//        System.out.println("Nombre: " + alumnoEncontrado.getNombre());
+//        System.out.println("DNI: " + alumnoEncontrado.getDni());
     }
 
-    }
+    
     
 }
