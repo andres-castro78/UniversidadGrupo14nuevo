4662193d7d06df18b812413117ec94d626672a0a
L3tt1 <barboza.leticia@gmail.com>
2023-09-13 15:41:52 -0300

    Metodos en MateriaData
    Metodos eliminar, modificar

--- src/accesoADatos/MateriaData.java
+++ src/accesoADatos/MateriaData.java
@@ -1,42 +1,123 @@
-
 package accesoADatos;
 
 import entidades.Materia;
 import java.sql.*;
 import javax.swing.JOptionPane;
 
-
 public class MateriaData {
-    
+
     private Connection con = null;
-    
+
     public MateriaData() {
         con = Conexion.getConexion();
     }
-    
-    public void guardarMateria(Materia materia){
+
+    public void guardarMateria(Materia materia) {
         String sql = "INSERT INTO materia (nombre, año, estado)"
                 + "VALUES(?,?,?)";
-        
+
         try {
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, materia.getNombre());
             ps.setInt(2, materia.getAnioMateria());
             ps.setBoolean(3, materia.isActivo());
             ps.executeUpdate();
-            
+
             ResultSet rs = ps.getGeneratedKeys();
-            
-            if (rs.next()){
-                
+
+            if (rs.next()) {
+
                 materia.setIdMateria(rs.getInt(1));
-               JOptionPane.showMessageDialog(null, "Materia guardada exitosamente.");
+                JOptionPane.showMessageDialog(null, "Materia guardada exitosamente.");
+
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia." + ex);
+        }
+    }
+
+    public Materia buscarMateria(int id) {
+
+        String sql = "SELECT nombre, año, estado FROM materia"
+                + "WHERE idMateria = ? AND estado = 1";
+
+        Materia materia = null;
+
+        try {
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()){
                 
+               materia = new Materia();
+               materia.setIdMateria(id);
+               materia.setNombre(rs.getString("nombre"));
+               materia.setAnioMateria(rs.getInt("año"));
+               materia.setActivo(true);
+                
+                
+            }else{
+                JOptionPane.showMessageDialog(null, "No existe una materia con el Id indicado o no está activa.");
             }
             ps.close();
             
+            
+            
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia."+ex);
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia." + ex);
+
         }
+        return materia;
+
     }
+    
+    public void modificarMateria(Materia materia){
+        String sql = "UPDATE materia SET nombre =?, año =?"
+                + "WHERE idMateria = ?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnioMateria());
+            
+            int exito = ps.executeUpdate();
+            
+            if (exito == 1){
+                JOptionPane.showMessageDialog(null, "Materia modificada");
+                
+            }
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia." + ex);
+        }
+               
+    }
+     
+    public void eliminarMateria(int id){
+            
+        String sql = "UPDATE materia SET estado = 0 WHERE idMateria = ?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int exito = ps.executeUpdate();
+            
+            if (exito == 1) {
+                
+                JOptionPane.showMessageDialog(null, "Materia Eliminada");
+            }
+            
+            
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia." + ex);
+        }
+        
+        
+        }
+
 }
--- src/universidadgrupo14nuevo/UniversidadGrupo14nuevo.java
+++ src/universidadgrupo14nuevo/UniversidadGrupo14nuevo.java
@@ -2,16 +2,18 @@
 package universidadgrupo14nuevo;
 
 import accesoADatos.AlumnoData;
+import accesoADatos.MateriaData;
 import entidades.Alumno;
+import entidades.Materia;
 import java.time.LocalDate;
 
 public class UniversidadGrupo14nuevo {
 
     public static void main(String[] args) {
         
-       Alumno juan = new Alumno (1, 111111111, "Luna", "Juan Pedro",LocalDate.of(1970, 5 , 17),false);
-       AlumnoData alu = new AlumnoData();
-       alu.guardarAlumno(juan); 
+//       Alumno juan = new Alumno (1, 111111111, "Luna", "Juan Pedro",LocalDate.of(1970, 5 , 17),false);
+//       AlumnoData alu = new AlumnoData();
+//       alu.guardarAlumno(juan); 
       //alu.modificarAlumno(juan);
       //alu.eliminarAlumno(1);
 //       Alumno alumnoEncontrado = alu.buscarAlumno(5);
@@ -19,6 +21,13 @@ public class UniversidadGrupo14nuevo {
 //        System.out.println("Apellido: " + alumnoEncontrado.getApellido());
 //        System.out.println("Nombre: " + alumnoEncontrado.getNombre());
 //        System.out.println("DNI: " + alumnoEncontrado.getDni());
+        Materia matem = new Materia ("Matemática", 2, true);
+        MateriaData materia = new MateriaData();
+        
+        materia.guardarMateria(matem);
+
+
+
     }
 
     
